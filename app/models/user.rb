44057315a853b553ff7404e38fab1a30efbaede7
Aaron Onken <aarononken@gmail.com>
2014-11-11 15:29:23 +0000

    make it better

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  validates :username, :email, :password, uniqueness: true
+  validates :username, :email, presence: true
+end
