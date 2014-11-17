8f50c1317c4b70bb0eff0217a2105b1ef59bf6f9
Aaron Onken <aarononken@gmail.com>
2014-11-17 01:09:37 +0000

    Added voting to links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,5 +1,17 @@
 class LinksController < ApplicationController
 
+  def upvote
+    @link = Link.find(params[:id])
+    @link.upvote_by current_user
+    redirect_to links_path
+  end
+  
+  def downvote
+    @link = Link.find(params[:id])
+    @link.downvote_by current_user
+    redirect_to links_path
+  end
+  
 	def index
 		@links = Link.all
 	end
